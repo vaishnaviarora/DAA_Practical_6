1970a2e8d85b526a9c373a2954b718caaa0f9b6e
vaishnaviarora <123967735+vaishnaviarora@users.noreply.github.com>
2023-02-25 11:40:52 +0530

    Add files via upload

---
+++ bfs.java
@@ -0,0 +1,54 @@
+import java.util.*; 
+
+class bfs { 
+
+	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) 
+	{ 
+		adj.get(u).add(v); 
+		// adj.get(v).add(u); 
+	} 
+
+	static void BFS(ArrayList<ArrayList<Integer>> adj,int V,int s) 
+	{ 
+		boolean[] visited=new boolean[V]; 
+    	for(int i = 0; i < V; i++) 
+    		visited[i] = false; 
+    
+    	LinkedList<Integer> q=new LinkedList<>();
+    	
+    	visited[s] = true; 
+    	q.add(s); 
+    
+    	while(q.isEmpty()==false) 
+    	{ 
+    		int u = q.poll(); 
+    		System.out.print(u + " "); 
+    		 
+    		for(int v:adj.get(u)){
+    		    if(visited[v]==false){
+    		        visited[v]=true;
+    		        q.add(v);
+    		    }
+    		} 
+    	} 
+	} 
+
+	public static void main(String[] args) 
+	{  
+		int V = 5; 
+		ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer>>(V); 
+		
+		for (int i = 0; i < V; i++) 
+			adj.add(new ArrayList<Integer>()); 
+
+		addEdge(adj,0,1); 
+    	addEdge(adj,0,3); 
+    	addEdge(adj,1,2); 
+    	addEdge(adj,2,3); 
+    	addEdge(adj,1,3);
+    	addEdge(adj,3,4);
+    	addEdge(adj,2,4);
+		
+	    BFS(adj,V,0);
+	} 
+} 
